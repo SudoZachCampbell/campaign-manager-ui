9f0d37fdb0fecccaa75dce5868fce044b294fb98
40132243 <zcam1995@gmail.com>
2020-05-16 15:31:17 +0100

    Added new appbar

---
+++ src/components/NavMenu.tsx
@@ -0,0 +1,183 @@
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import { Link } from 'react-router-dom';
+import {
+    AppBar,
+    CssBaseline,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemProps,
+    ListItemText,
+    Toolbar,
+    Typography
+} from '@material-ui/core';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles, useTheme, Theme } from '@material-ui/core/styles';
+import './NavMenu.css';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    },
+}));
+
+export default function NavMenu(props) {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar
+                position="static"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(classes.menuButton, open && classes.hide)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        Persistent drawer
+            </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </div>
+                <Divider />
+                {/* 
+                TODO: Added Recently Visited Pages (linked to user)
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List> */}
+                {/* <Divider /> */}
+                <List>
+                        <ListItemLink href='/players'>
+                            <ListItemIcon></ListItemIcon>
+                            <ListItemText primary='Players' />
+                        </ListItemLink>
+                        <ListItemLink href='/npcs'>
+                            <ListItemIcon></ListItemIcon>
+                            <ListItemText primary='Npcs' />
+                        </ListItemLink>
+                        <ListItemLink href='/places'>
+                            <ListItemIcon></ListItemIcon>
+                            <ListItemText primary='Places' />
+                        </ListItemLink>
+                </List>
+            </Drawer>
+        </div>
+        //       <ul className="navbar-nav flex-grow">
+        //       <NavItem>
+        //           <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+        //       </NavItem>
+        //       <NavItem>
+        //           <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+        //       </NavItem>
+        //       <NavItem>
+        //           <NavLink tag={Link} className="text-dark" to="/characters">Characters</NavLink>
+        //       </NavItem>
+        //       <NavItem>
+        //           <NavLink tag={Link} className="text-dark" to="/npcs">Npcs</NavLink>
+        //       </NavItem>
+        //   </ul>
+    )
+}
+
+function ListItemLink(props: ListItemProps<'a', { button?: true }>) {
+    return <ListItem button component="a" {...props} />;
+}
