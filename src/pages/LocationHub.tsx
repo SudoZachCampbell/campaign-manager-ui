162250ba6ba31a10fac35b183c3f2ee9c98a8cb4
Zach Campbell <zcam1995@gmail.com>
2020-06-11 17:31:06 +0100

    Added Phan map and marker

--- src/pages/LocationHub.tsx
+++ src/pages/LocationHub.tsx
@@ -6,25 +6,6 @@ import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 export default function LocationHub(props) {
-    // const bounds = [[0, 0], [1095, 800]]
-
-    // return (
-    //     <Map
-
-    //         center={[100,0]}
-    //         bounds={bounds}
-    //         minZoom={2}>
-    //         <ImageOverlay
-    //             url='https://ddimagecollection.s3-eu-west-1.amazonaws.com/maps/AreaReduced.jpg
-    //             '
-    //             bounds={bounds} />
-    //         <Marker position={[100, 100]}>
-    //             <Popup>
-    //                 A Pretty Popup
-    //                 </Popup>
-    //         </Marker>
-    //     </Map>
-    // );
 
     let DefaultIcon = L.icon({
         iconUrl: icon,
@@ -33,19 +14,19 @@ export default function LocationHub(props) {
     
     L.Marker.prototype.options.icon = DefaultIcon;
 
-    const bounds = [[0, 0], [1095, 800]]
+    const bounds = [[0, 0], [4734, 7000]]
     const style = { height: '80vh', width: '75vw' }
 
     return (
-        <Map crs={L.CRS.Simple} minZoom={0} bounds={bounds} style={style}>
-            <Marker position={[bounds[1][0] - 852, 458]}>
+        <Map crs={L.CRS.Simple} minZoom={-3} bounds={bounds} style={style}>
+            <Marker position={[bounds[1][0] - 1830, 3083]}>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
             </Marker>
             <ImageOverlay
                 bounds={bounds}
-                url='https://ddimagecollection.s3-eu-west-1.amazonaws.com/maps/AreaReduced.jpg'
+                url='https://ddimagecollection.s3-eu-west-1.amazonaws.com/maps/PhanDay.jpg'
             />
 
         </Map>
