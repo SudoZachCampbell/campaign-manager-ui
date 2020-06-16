f83df446b8f6b1fd6d71b11304e8d6e10500216c
Zach Campbell <zcam1995@gmail.com>
2020-06-16 13:07:42 +0100

    Added Circle, testing center

--- src/pages/LocationHub.tsx
+++ src/pages/LocationHub.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Map, Marker, TileLayer, Popup, ImageOverlay } from 'react-leaflet'
+import { Circle, Map, Marker, TileLayer, Popup, ImageOverlay } from 'react-leaflet'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css';
 import icon from '../assets/icons/person.svg';
+import { Box } from '@material-ui/core';
 
 export default function LocationHub(props) {
 
@@ -12,23 +13,25 @@ export default function LocationHub(props) {
         iconAnchor: [20, 40],
         popupAnchor: [0, -35],
         iconSize: [60, 60]
-    }) 
+    })
 
     const bounds = [[0, 0], [4734, 7000]]
     const style = { height: '80vh', width: '75vw' }
 
     return (
-        <Map crs={L.CRS.Simple} minZoom={-3} bounds={bounds} style={style}>
-            <Marker icon={personIcon} position={[bounds[1][0] - 1830, 3083]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-            </Marker>
-            <ImageOverlay
-                bounds={bounds}
-                url='https://ddimagecollection.s3-eu-west-1.amazonaws.com/maps/PhanDay.jpg'
-            />
-
-        </Map>
+        <Box display='flex' justifyContent='center'>
+            <Map crs={L.CRS.Simple} center={[0, 0]} minZoom={-2} bounds={bounds} style={style}>
+                <Marker icon={personIcon} position={[bounds[1][0] - 1830, 3083]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+                <ImageOverlay
+                    bounds={bounds}
+                    url='https://ddimagecollection.s3-eu-west-1.amazonaws.com/maps/PhanDay.jpg'
+                />
+                <Circle center={[0, 0]} radius={200} fillColor='blue' />
+            </Map>
+        </Box>
     )
 }
